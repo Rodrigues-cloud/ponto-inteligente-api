85b2a6b1b6c64d5fd16f4c95b19099c3a0e94af4
Rodrigues-cloud <uesleyrs2020@outlook.com>
2020-08-11 15:56:33 -0300

    comite inicial dos arquivos do projeto

---
+++ src/test/java/com/uesley/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uesley.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
